8da1ae9a21b280862ca9493bf945c113a2861faa
Aandres831 <andresseverino646@gmail.comm>
2025-11-06 13:34:57 -0500

    update develop branch

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <>
+            <div className='header-page-dashboard'>
+                <nav className='navbar-dashboard'>
+                    <div className='title-navbar-dashboard'>
+                        <p>
+                            MoreciShop
+                        </p>
+                    </div>
+                    <div className='list-navbar-home'>
+                        <ul>
+                            <li>Home</li>
+                            <li>Products</li>
+                            <li>About</li>
+                        </ul>
+                    </div>
+                    <div className='icons-navbar-dashboard'>
+                        
+                        <p>C</p>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
